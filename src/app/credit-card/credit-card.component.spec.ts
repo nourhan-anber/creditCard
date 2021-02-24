34b60915ef1e273a7203079de8f598bf46a1ea7c
Nourhan Anber <nourhan.anber@gmail.com>
2021-02-24 09:36:11 -0500

    front end unit test added

--- src/app/credit-card/credit-card.component.spec.ts
+++ src/app/credit-card/credit-card.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreditCardComponent } from './credit-card.component';
+import HttpService from '../service/http.service';
+import EncryptService from '../service/encrypt.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('CreditCardComponent', () => {
   let component: CreditCardComponent;
@@ -8,7 +11,9 @@ describe('CreditCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreditCardComponent ]
+      declarations: [ CreditCardComponent],
+      imports: [HttpClientTestingModule],
+      providers: [ HttpService, EncryptService]
     })
     .compileComponents();
   }));
@@ -22,4 +27,36 @@ describe('CreditCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should accept only numbers starts with 4, 5 and 37', ()=>{
+    component.creditCardForm.controls.creditCardNumber.setValue("11111111111111");
+    component.creditCardForm.markAsDirty();
+    expect(component.creditCardForm.controls.creditCardNumber.errors.invalid).toBeTruthy();
+  })
+  it('should acceept numbers that betweeen 13 and 16 digits', ()=>{
+    component.creditCardForm.controls.creditCardNumber.setValue("41111111777111");
+    component.creditCardForm.markAsDirty();
+    expect(component.creditCardForm.controls.creditCardNumber.errors).toBeNull();
+  })
+  it('should return visa if the number starts with 4', ()=>{
+    component.creditCardForm.controls.creditCardNumber.setValue("41111111777111");
+    component.creditType = "";
+    component.setCreditCardType();
+    expect(component.creditType).toBe("Visa");
+
+  })
+  it('should return master card if the number starts with 5', ()=>{
+    component.creditCardForm.controls.creditCardNumber.setValue("51111111777111");
+    component.creditType = "";
+    component.setCreditCardType();
+    expect(component.creditType).toBe("Master");
+
+  })
+  it('should return ameerican express if the number starts with 37', ()=>{
+    component.creditCardForm.controls.creditCardNumber.setValue("37111111777111");
+    component.creditType = "";
+    component.setCreditCardType();
+    expect(component.creditType).toBe("AmericanExpress");
+
+  })
 });
